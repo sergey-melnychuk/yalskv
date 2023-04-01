3a7af4d1378bc611a528fabdc3fcde3130d2f477
Sergey Melnychuk <serg.meln@gmail.com>
2023-04-01 15:11:19 +0000

    use real numbers

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ YALSKV
 ```
 $ cargo clean && cargo build --release && ./target/release/main
 <snip>
-n=1000000 ms=25161 op=79488 kb=7452
+N=1000000 limit=33554432
+insert: ok (ms=5847 op=171027 kb=25386)
+reduce: ok (ms=14600 op=68493 kb=10166)
+lookup: ok (ms=1775 op=563380 kb=83626)
+sorted: ok (ms=1565 op=638977 kb=94848)
+remove: ok (ms=4110 op=243309 kb=36116)
+reduce: ok (ms=15332 op=65223 kb=9681)
 ```
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 use yalskv::util::{data, hex, mix};
-use yalskv::{kv, Record, Store};
+use yalskv::{kv, Store};
 
 use std::time::SystemTime;
 
@@ -8,9 +8,11 @@ fn main() -> kv::Result<()> {
     let mut store = Store::open("target/db")?;
 
     const N: usize = 1000000;
-    let now = SystemTime::now();
-
+    let limit = 1024 * 1024 * 32;
     let data = data(N, 42);
+    println!("N={N} limit={limit}");
+
+    let mut now = SystemTime::now();
     for (key, val) in data.iter() {
         store.insert(key, val)?;
     }
@@ -18,60 +20,87 @@ fn main() -> kv::Result<()> {
     let ms = (now.elapsed().unwrap().as_millis() as usize).max(1);
     let op = N * 1000 / ms;
     let kb = N * 1000 * (64 + 64 + 3 * 8) / ms / 1024;
-    println!("n={} ms={} op={} kb={}", N, ms, op, kb);
-    println!("insert: ok");
+    println!("insert: ok (ms={ms} op={op} kb={kb})");
 
-    let limit = 1024 * 1024 * 32;
+    now = SystemTime::now();
     store.reduce(limit)?;
-    println!("reduce: ok");
+    let ms = (now.elapsed().unwrap().as_millis() as usize).max(1);
+    let op = N * 1000 / ms;
+    let kb = N * 1000 * (64 + 64 + 3 * 8) / ms / 1024;
+    println!("reduce: ok (ms={ms} op={op} kb={kb})");
 
     let data = mix(data, 1);
-    for (key, value) in data.iter() {
-        if let Some(stored) = store.lookup(key)? {
-            if &stored != value {
-                eprintln!("!match: key={}", hex(key));
-            }
-        } else {
+    now = SystemTime::now();
+    let mut found = Vec::with_capacity(data.len());
+    for (key, _) in data.iter() {        
+        let val = store.lookup(key)?.unwrap_or_default();
+        found.push(val);
+    }
+    let ms = (now.elapsed().unwrap().as_millis() as usize).max(1);
+    let op = N * 1000 / ms;
+    let kb = N * 1000 * (64 + 64 + 3 * 8) / ms / 1024;
+    println!("lookup: ok (ms={ms} op={op} kb={kb})");
+    for ((key, val), res) in data.iter().zip(found.iter()) {        
+        if res.is_empty() {
             eprintln!("!found: key={}", hex(key));
+        } else if val != res {
+            eprintln!("!match: key={}", hex(key));
         }
     }
-    println!("lookup: ok");
 
+    now = SystemTime::now();
     store.file().reset()?;
-    let mut prev: Option<Record> = None;
-    for (i, next) in store.file().enumerate() {
-        if prev.is_none() {
-            prev = Some(next);
+    let mut found = Vec::with_capacity(data.len());
+    for rec in store.file() {
+        found.push(rec.key().to_vec());
+    }
+    let ms = (now.elapsed().unwrap().as_millis() as usize).max(1);
+    let op = N * 1000 / ms;
+    let kb = N * 1000 * (64 + 64 + 3 * 8) / ms / 1024;
+    println!("sorted: ok (ms={ms} op={op} kb={kb})");
+
+    let mut prev: Vec<u8> = Default::default();
+    for (i, next) in found.into_iter().enumerate() {
+        if prev.is_empty() {
+            prev = next;
             continue;
         }
 
-        if prev.as_ref().unwrap().key() > next.key() {
+        if prev > next {
             println!(
                 "!sorted (i={}):\n\tprev={}\n\tnext={}",
                 i,
-                hex(prev.as_ref().unwrap().key()),
-                hex(next.key())
+                hex(&prev),
+                hex(&next)
             );
         }
-        prev = Some(next);
+        prev = next;
     }
-    println!("sorted: ok");
 
-    store.file().unset()?;
     let data = mix(data, 2);
+    now = SystemTime::now();
+    store.file().unset()?;
     for (key, _) in data.iter() {
         if !store.remove(key)? {
             eprintln!("!exist: key={}", hex(key));
         }
     }
+    let ms = (now.elapsed().unwrap().as_millis() as usize).max(1);
+    let op = N * 1000 / ms;
+    let kb = N * 1000 * (64 + 64 + 3 * 8) / ms / 1024;
+    println!("remove: ok (ms={ms} op={op} kb={kb})");
+
+    now = SystemTime::now();
     store.reduce(limit)?;
+    let ms = (now.elapsed().unwrap().as_millis() as usize).max(1);
+    let op = N * 1000 / ms;
+    let kb = N * 1000 * (64 + 64 + 3 * 8) / ms / 1024;
+    println!("reduce: ok (ms={ms} op={op} kb={kb})");
 
     store.file().reset()?;
     let count = store.file().count();
     if count > 0 {
         eprintln!("!empty: {}", count);
     }
-    println!("remove: ok");
-
     Ok(())
 }
